311fa46043fda0fe01cae3bfedd21cbfb0f1e382
fernandes-draw <fernandes_draw@hotmail.com>
2025-05-09 21:18:29 -0300

    Next.js 15 - Aula_31 até Aula_34

--- app/complex-dashboard/layout.tsx
+++ app/complex-dashboard/layout.tsx
@@ -3,22 +3,28 @@ export default function ComplexDashboardLayout({
   users,
   revenue,
   notifications,
+  login,
 }: {
   children: React.ReactNode;
   users: React.ReactNode;
   revenue: React.ReactNode;
   notifications: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
+  const isLoggedIn = true;
+
+  return isLoggedIn ? (
     <div>
       <div>{children}</div>
-      <div style={{ display: 'flex' }}>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div style={{ display: "flex" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <div>{users}</div>
           <div>{revenue}</div>
         </div>
-        <div style={{ display: 'flex', flex: 1 }}>{notifications}</div>
+        <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
       </div>
     </div>
+  ) : (
+    login
   );
 }
---
+++ app/f4/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function F4() {
+  return <h1>F4 page</h1>;
+}
